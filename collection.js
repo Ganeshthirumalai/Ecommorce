6473f901f67b4e46d2acc83fe94e5182c553e957
Ganeshthirumalai <156631877+Ganeshthirumalai@users.noreply.github.com>
2024-02-09 18:11:16 +0530

    Update collection.js

--- collection.js
+++ collection.js
@@ -13,3 +13,13 @@ search.addEventListener("keyup", function () {
         }
     }
 });
+var sidenav = document.querySelector(".side-navbar");
+
+function showNavbar() {
+    sidenav.style.left = "0";
+}
+
+function closeNavbar() {
+    sidenav.style.left= "-60%";
+}
+
